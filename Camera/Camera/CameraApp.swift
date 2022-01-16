4f1341210ca1fcf919e5228dcafd1f1af25a9abe
ZhongUncle <2020738339@qq.com>
2022-01-16 21:43:03 +0800

    Add New Project Camera

---
+++ Camera/Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by 钟宜江 on 2022/1/16.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
